b4e1b9cd96b73af8c31140526148fdbff3e61c0c
Dan Hall <dhall0803@gmail.com>
2022-05-07 15:19:58 +0100

    Add -y to apt install

--- install_aws_cli_ubuntu.sh
+++ install_aws_cli_ubuntu.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 sudo apt update
-sudo apt install zip
+sudo apt install zip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
