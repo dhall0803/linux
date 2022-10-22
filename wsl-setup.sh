afb771bf7514d12380e915e7760bef72ab2594fc
dhall0803 <dhall0803@gmail.com>
2022-10-22 21:35:58 +0100

    Update wsl-setup.sh

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo
 echo "###################################"
 echo "Running WSL Setup Script"
 echo "###################################"
 echo
 
+echo
 echo "###################################"
 echo "Updating Packages"
 echo "###################################"
@@ -11,6 +13,7 @@ echo
 
 sudo apt-get update && sudo apt upgrade -y
 
+echo
 echo "###################################"
 echo "Installing Common Utilities"
 echo "###################################"
@@ -18,6 +21,7 @@ echo
 
 sudo apt-get install -y gnupg software-properties-common zip wget apt-transport-https git python3-venv python3-pip
 
+echo
 echo "###################################"
 echo "Installing Terraform"
 echo "###################################"
@@ -27,12 +31,14 @@ wget -O- https://apt.releases.hashicorp.com/gpg | \
     gpg --dearmor | \
     sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
+echo
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
     https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
     sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt-get install terraform
 
+echo
 echo "###################################"
 echo "Installing tflint"
 echo "###################################"
@@ -40,6 +46,7 @@ echo
 
 curl -s https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
 
+echo
 echo "###################################"
 echo "Installing az-cli"
 echo "###################################"
@@ -47,6 +54,7 @@ echo
 
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
+echo
 echo "###################################"
 echo "Installing Powershell"
 echo "###################################"
@@ -57,6 +65,7 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get install -y powershell
 
+echo
 echo "###################################"
 echo "Installing Python 3.10"
 echo "###################################"
@@ -65,6 +74,7 @@ echo
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.10 -y
 
+echo
 echo "###################################"
 echo "Installing .NET Core 6"
 echo "###################################"
@@ -76,6 +86,7 @@ rm packages-microsoft-prod.deb
 
 sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0
 
+echo
 echo "###################################"
 echo "Installing NVM"
 echo "###################################"
